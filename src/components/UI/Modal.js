552a588e1240d24443c208d81991243679c6c3c3
Shambhavi Dinakar <shambhavidinakar@Shambhavis-MBP.fios-router.home>
2022-02-16 20:40:14 -0500

    adding Portals for modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,25 +1,34 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import styles from "./Modal.module.css";
 import Card from "./Card";
 import Button from "./Button";
 
-const Modal = (props) => {
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onCancel} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <>
-      <div className={styles.backdrop} onClick={props.onCancel} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>Error</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onCancel}>Okay!</Button>
-        </footer>
-      </Card>
-    </>
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>Error</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onCancel}>Okay!</Button>
+      </footer>
+    </Card>
   );
+};
+
+const Modal = (props) => {
+  return <>
+  {ReactDOM.createPortal(<Backdrop onCancel={props.onCancel}/>, document.getElementById('backdrop-root'))}
+  {ReactDOM.createPortal(<ModalOverlay onCancel={props.onCancel} message={props.message}/>, document.getElementById('overlay-root'))}
+  </>;
 };
 
 export default Modal;
